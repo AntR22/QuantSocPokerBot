828a7db56d00273d495d0295752fb76e5ff1eead
PureFate <derrianauduong@gmail.com>
2024-09-06 21:33:48 +1000

    Changed example bot to account for raising capability
    Added check to see if player is able to raise, if not able to they call or check.

--- example_bot.py
+++ example_bot.py
@@ -6,17 +6,20 @@ class example_bot(poker_player):
         self.name = "example bot"
     
     def decide_action(self, game_state: game_state):
+        current_bet = game_state.get_current_bet()
+
         # Check if the bot has a pair in its hand
         if self.has_pair(game_state.get_my_cards()):
-            # If the bot has a pair, it decides to raise
-            return ('raise', game_state.get_min_bet())
+            # Check player stack for raise
+            if game_state.get_my_stack() >= game_state.min_bet + current_bet:
+                # If the bot has a pair, it decides to raise
+                return ('raise', game_state.get_min_bet())
+        
+        # Otherwise, the bot decides to limp (call the current bet)
+        if current_bet > 0:
+            return ('call', current_bet)
         else:
-            # Otherwise, the bot decides to limp (call the current bet)
-            current_bet = game_state.get_current_bet()
-            if current_bet > 0:
-                return ('call', current_bet)
-            else:
-                return ('check', 0)  # No bet to call, just check
+            return ('check', 0)  # No bet to call, just check
 
     def has_pair(self, cards):
         """
@@ -25,4 +28,4 @@ class example_bot(poker_player):
         :return: True if the bot has a pair, False otherwise.
         """
         # Check if the two hole cards have the same rank (pair)
-        return cards[0].rank == cards[1].rank
+        return cards[0] == cards[1]
