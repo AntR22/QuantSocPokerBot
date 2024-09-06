f7f8f85537b6e22baa6e2d6563b626d7f4c2e19b
AntR22 <antonragusa0@gmail.com>
2024-09-06 14:55:44 +1000

    update documentation

--- poker_game.py
+++ poker_game.py
@@ -20,6 +20,64 @@ class poker_game:
         self.current_dealer_index = 0
         self.river = []
         self.winner = None
+        self.has_folded = [False, False]
+    
+    def do_betting_round(self, small_blind, big_blind):
+        while True:
+            # small blind first response
+            try:
+                (action1, amount1) = self.players[small_blind].decide_action(game_state(small_blind, self))
+            except Exception as e:
+                print("error in decide_action for player: ", self.players[small_blind].get_name(), "\n\nthe error:\n", e)
+                action1 = "fold"
+                
+            # checks correct usage of the action string, also checks bet amount is allowed
+            if action1 != "fold" and action1 != "check" and action1 != "call" and action1 != "raise":
+                print("invalid action for player: ", self.players[small_blind].get_name())
+                action1 = "fold"
+            elif amount1 > self.stacks[small_blind]:
+                print("player does not have enough chips for that bet! player: ", self.players[small_blind].get_name())
+                action1 = "fold"
+
+            if amount1 != 0 and (action1 == "fold" or action1 == "check"):
+                print("invalid action (bet given on a fold or check) for player: ", self.players[small_blind].get_name())
+                action1 = "fold"
+
+
+
+
+            # dealer second response
+            try:
+                (action2, amount2) = self.players[big_blind].decide_action(game_state(big_blind, self))
+            except Exception as e:
+                print(f"error in decide_action for player: ", self.players[small_blind].get_name(), "\n\nthe error:\n", e)
+                # TODO then auto fold
+                
+            # checks correct usage of the action string, also checks bet amount is allowed
+            if action2 != "fold" and action2 != "check" and action2 != "call" and action2 != "raise":
+                print("invalid action for player: ", self.players[small_blind].get_name())
+                # TODO then auto fold
+            elif amount2 > self.stacks[big_blind]:
+                print("player does not have enough chips for that bet! player: ", self.players[small_blind].get_name())
+                # TODO then auto fold
+
+            if amount2 != 0 and (action2 == "fold" or action2 == "check"):
+                print("invalid action (bet given on a fold or check) for player: ", self.players[small_blind].get_name())
+                action1 = "fold"
+
+
+            
+            if action1 == "check" and action2 == "check":
+                #sanity check
+                if self.bets[0] != self.bets[1]:
+                    print("!!huge error, bets are unequal when both players have checked!!\n")
+                    exit(1)
+                self.pot += self.bets[0]
+                self.pot += self.bets[1]
+                self.bets[0] = 0
+                self.bets[1] = 0
+                return True
+                
     
     def play(self, num_rounds: int):
         round_num = 1
@@ -51,24 +109,9 @@ class poker_game:
             card4 = self.deck.get_rand_card()
             self.cards[dealer_index][1] = card4
             
-            # Pre flop betting phase actions TODO complete the logic below
-            while True:
-                (action1, amount1) = self.players[small_blind].decide_action(game_state(small_blind, self))
-                # checks correct usage of the action string, TODO probably should also check bet amount is allowed
-                if action1 != "fold" and action1 != "check" and action1 != "call" and action1 != "raise":
-                    print("invalid action for player: ", self.players[small_blind].get_name())
-                    # TODO then auto fold
-                
-                (action2, amount2) = self.players[big_blind].decide_action(game_state(big_blind, self))
-                # checks correct usage of the action string, TODO probably should also check bet amount is allowed
-                if action2 != "fold" and action2 != "check" and action2 != "call" and action2 != "raise":
-                    print("invalid action for player: ", self.players[small_blind].get_name())
-                    # TODO then auto fold
-                    
-                if action1 == "check" and action2 == "check":
-                    # TODO actions here
-                    break
-                
+            # Pre flop betting phase actions 
+            pfresult = self.do_betting_round(small_blind, big_blind)
+            
             # Flop phase
             # burn card before flop
             self.deck.burn_card()
@@ -81,47 +124,19 @@ class poker_game:
             self.river.append(river2)
             self.river.append(river3)
             
-            # flop betting phase actions TODO complete the logic below
-            while True:
-                (action1, amount1) = self.players[small_blind].decide_action(game_state(small_blind, self))
-                # checks correct usage of the action string, TODO probably should also check bet amount is allowed
-                if action1 != "fold" and action1 != "check" and action1 != "call" and action1 != "raise":
-                    print("invalid action for player: ", self.players[small_blind].get_name())
-                    # TODO then auto fold
-                
-                (action2, amount2) = self.players[big_blind].decide_action(game_state(big_blind, self))
-                # checks correct usage of the action string, TODO probably should also check bet amount is allowed
-                if action2 != "fold" and action2 != "check" and action2 != "call" and action2 != "raise":
-                    print("invalid action for player: ", self.players[small_blind].get_name())
-                    # TODO then auto fold
-                    
-                if action1 == "check" and action2 == "check":
-                    # TODO actions here
-                    break
-                
+            # flop betting phase actions 
+            if pfresult:
+                fresult = self.do_betting_round(small_blind, big_blind)
+
             # Turn phase 
             # add card to river
             self.deck.burn_card()
             turncard = self.deck.get_rand_card()
             self.river.append(turncard)
             
-            # TODO logic for the turn phase betting
-            while True:
-                (action1, amount1) = self.players[small_blind].decide_action(game_state(small_blind, self))
-                # checks correct usage of the action string, TODO probably should also check bet amount is allowed
-                if action1 != "fold" and action1 != "check" and action1 != "call" and action1 != "raise":
-                    print("invalid action for player: ", self.players[small_blind].get_name())
-                    # TODO then auto fold
-                
-                (action2, amount2) = self.players[big_blind].decide_action(game_state(big_blind, self))
-                # checks correct usage of the action string, TODO probably should also check bet amount is allowed
-                if action2 != "fold" and action2 != "check" and action2 != "call" and action2 != "raise":
-                    print("invalid action for player: ", self.players[small_blind].get_name())
-                    # TODO then auto fold
-                    
-                if action1 == "check" and action2 == "check":
-                    # TODO actions here
-                    break
+            # turn phase betting
+            if pfresult and fresult:
+                tresult = self.do_betting_round(small_blind, big_blind)
             
             # River phase 
             # add card to river
@@ -129,23 +144,9 @@ class poker_game:
             rivercard = self.deck.get_rand_card()
             self.river.append(rivercard)
             
-            # TODO logic for the river phase betting
-            while True:
-                (action1, amount1) = self.players[small_blind].decide_action(game_state(small_blind, self))
-                # checks correct usage of the action string, TODO probably should also check bet amount is allowed
-                if action1 != "fold" and action1 != "check" and action1 != "call" and action1 != "raise":
-                    print("invalid action for player: ", self.players[small_blind].get_name())
-                    # TODO then auto fold
-                
-                (action2, amount2) = self.players[big_blind].decide_action(game_state(big_blind, self))
-                # checks correct usage of the action string, TODO probably should also check bet amount is allowed
-                if action2 != "fold" and action2 != "check" and action2 != "call" and action2 != "raise":
-                    print("invalid action for player: ", self.players[small_blind].get_name())
-                    # TODO then auto fold
-                    
-                if action1 == "check" and action2 == "check":
-                    # TODO actions here
-                    break
+            # river phase betting
+            if pfresult and fresult and tresult:
+                rresult = self.do_betting_round(small_blind, big_blind)
             
             #TODO finish the round logic 
             # maybe call check_winning_hand() and fix up that function
@@ -160,7 +161,8 @@ class poker_game:
             return 1
         else:
             return None
-            
+
+
 def check_winning_hand(hand1, hand2, river):
     hand1_cards = copy.deepcopy(hand1 + river) # copy hand1 cards into a single array
     hand2_cards = copy.deepcopy(hand2 + river) # copy hand2 cards into a single array
--- poker_player.py
+++ poker_player.py
@@ -4,7 +4,7 @@ class poker_player:
     def __init__(self):
         self.name = "none"
 
-    def decide_action(self, game_state): 
+    def decide_action(self, game_state: game_state): 
         """
         Method to decide the bot's next action.
         Must be implemented by all bots.
@@ -13,10 +13,13 @@ class poker_player:
         - gamestate: 
         
         Returns:
-        -   action: A tuple (action_type, amount), where action_type is one of 
-            'fold', 'check', 'call' or 'raise', and amount is the bet amount if raising (excluding current bet).
-        -   set amount to 0 if folding, checking or calling. Upon raising, make sure the raise amount value is greater than 
-            minbet found in the gamestate and not greater than your current stack 
+        -   action: A tuple (action_type, amount)
+        -   action_type: action_type is one of 'fold', 'check', 'call' or 'raise' - anything else will be treated as fold.
+        -   amount: amount is the bet amount if raising or calling (excluding your current bet). Set amount to 0 if folding or checking.
+            Upon raising, make sure the raise amount value is greater than minbet(unless all in) found in the gamestate 
+            and not greater than your current stack, to call DO NOT match your opponents bet UNLESS you have enough chips to do so, 
+            otherwise you will be autofolded, just call 'all in' (the rest of your stack), the backend will adjust the opposing bet
+            to match yours. Likewise if you raise above the remaining chips the opponent has left then your bet will be curtailed.
         """
         raise NotImplementedError("This method must be implemented by subclasses")
     
